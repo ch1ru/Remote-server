09a34fd210c1af14faf9f38232d271383b713450
ch1ru <caleb1@criptext.com>
2025-10-28 22:29:46 +0000

    report dir

--- client/hardware_menu.py
+++ client/hardware_menu.py
@@ -116,7 +116,6 @@ try:
                 wait_for_task(task_id, interval=1, verbose=True)
                 report_url = fastp_report(id)
                 img = qrcode.make(report_url)
-                img.save(f'/reports/fastp_report_{id}_qrcode.png')
                 device_menu.show_qr(img)
                 while True:
                     if not GPIO.input(device_menu.BUTTON_X):
@@ -131,7 +130,6 @@ try:
                 # show just R1 report as example
                 report_url = fastqc_report(id, filename='trimmed_anc_R1')
                 img = qrcode.make(report_url)
-                img.save(f'/reports/fastqc_report_{id}_qrcode.png')
                 device_menu.show_qr(img)
                 while True:
                     if not GPIO.input(device_menu.BUTTON_X):
