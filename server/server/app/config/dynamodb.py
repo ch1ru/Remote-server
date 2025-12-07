53640c60d57de18c9fb84dc6b83f50ac19f28619
ch1ru <caleb1@criptext.com>
2025-12-07 15:01:28 +0000

    added dynamo db connection

--- server/server/app/config/dynamodb.py
+++ server/server/app/config/dynamodb.py
@@ -2,7 +2,7 @@ import boto3
 from botocore.exceptions import ClientError
 
 # Create DynamoDB client
-dynamodb = boto3.resource('dynamodb', region_name='your_region')
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
 # Reference your table
 table = dynamodb.Table('Commands')
